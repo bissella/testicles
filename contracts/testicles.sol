b7e7b744659c721393f2268a6d61f69bc73cc650
bissella <alexmbissell@gmail.com>
2024-02-02 06:30:28 +1300

    bull T

--- contracts/testicles.sol
+++ contracts/testicles.sol
@@ -4,13 +4,13 @@ import {ERC20} from "@openzeppelin/contracts@5.0.1/token/ERC20/ERC20.sol";
 import {Ownable} from "./Ownable.sol"; // This is scraped form openzepplin with renounceOwnership function disabled.
 import {ERC20Permit} from "@openzeppelin/contracts@5.0.1/token/ERC20/extensions/ERC20Permit.sol";
 
-contract EmissionUnitToken is ERC20, Ownable, ERC20Permit {
+contract BullTToken is ERC20, Ownable, ERC20Permit {
     constructor(
         address initialOwner
     )
-        ERC20("New Zealand Emission Unit Token", "NZUC")
+        ERC20("New Zealand Bull T Token", "NZUC")
         Ownable(initialOwner)
-        ERC20Permit("New Zealand Emission Unit Token")
+        ERC20Permit("New Zealand Bull T Token")
     {}
 
     function mint(uint256 amount) public onlyOwner {
