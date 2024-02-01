617ae48c321bf7f9a1b495d2c444444cfd0e0ff5
bissella <alexmbissell@gmail.com>
2024-02-01 19:38:46 +1300

    remove burnable import and modified the burn contract

--- contracts/testicles.sol
+++ contracts/testicles.sol
@@ -2,24 +2,23 @@
 pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts@5.0.1/token/ERC20/ERC20.sol";
-import {ERC20Burnable} from "@openzeppelin/contracts@5.0.1/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "./Ownable.sol"; // This is scraped form openzepplin with renounceOwnership function disabled.
 import {ERC20Permit} from "@openzeppelin/contracts@5.0.1/token/ERC20/extensions/ERC20Permit.sol";
 
-contract Testical is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+contract MyToken is ERC20, Ownable, ERC20Permit {
     constructor(
         address initialOwner
     )
-        ERC20("Testical", "Balls")
+        ERC20("New Zealand Emission Unit Token", "NZUC")
         Ownable(initialOwner)
-        ERC20Permit("Testical")
+        ERC20Permit("New Zealand Emission Unit Token")
     {}
 
     function mint(uint256 amount) public onlyOwner {
         _mint(owner(), amount * 10 ** decimals()); //only mint in whole units to the contract owner. Contract owner must transfer to depositors.
     }
 
-    function burn(uint256 amount) public virtual override onlyOwner {
-        super.burn(amount * 10 ** decimals()); //only burn in whole units.
+    function burn(uint256 amount) public onlyOwner {
+        _burn(owner(), amount * 10 ** decimals()); //only burn in whole units.
     }
 }
